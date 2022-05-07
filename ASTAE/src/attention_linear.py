Bug in architecture: do average of class and distillation token before decoder

ASTAE/src/attention_linear.py
@@ -15,7 +15,7 @@ import copy
 import collections
 
 
-def pretrained_AST(input_tdim = 1024, audioset_only = False):
+def pretrained_AST(input_tdim = 1024, audioset_only = False,audioset_pretrain=True,imagenet_pretrain=True,tiny=False):
     pretrained_mdl_path = '../../pretrained_models/audioset_10_10_0.4593.pth'
     # get the frequency and time stride of the pretrained model from its name
     fstride, tstride = int(pretrained_mdl_path.split('/')[-1].split('_')[1]), int(pretrained_mdl_path.split('/')[-1].split('_')[2].split('.')[0])
@@ -25,10 +25,24 @@ def pretrained_AST(input_tdim = 1024, audioset_only = False):
     print("device: "+ str(device))
     #sd = torch.load(pretrained_mdl_path, map_location=device)
     #audio_model_ast = ast_model.ASTModel(input_tdim=input_tdim, fstride=fstride, tstride=tstride,model_size='tiny224')
-    if audioset_only:
-        pretrained_mdl_path = '../../audio-transformer/scv2/checkpoint-75.pth'
-    audio_model_ast = ast_model.ASTModel(input_tdim=input_tdim, fstride=fstride, tstride=tstride)
-    #audio_model = torch.nn.DataParallel(audio_model_ast)
+    #if audioset_only:
+    #    pretrained_mdl_path = '../../audio-transformer/scv2/checkpoint-75.pth'
+
+    if tiny:
+          audio_model_ast = ast_model.ASTModel(input_tdim=input_tdim, fstride=fstride, tstride=tstride,
+                                             audioset_pretrain=False,imagenet_pretrain=False,model_size="tiny224")
+          if audioset_pretrain and imagenet_pretrain:
+             pretrained_mdl_path = '../../pretrained_models/tiny_ast_checkpoint-115.pth'
+             sd = torch.load(pretrained_mdl_path, map_location=device)
+             audio_model_ast.load_state_dict(sd, strict=False)
+
+          else:
+              raise ("not implemented")
+
+    else:
+        audio_model_ast = ast_model.ASTModel(input_tdim=input_tdim, fstride=fstride, tstride=tstride,
+                                             audioset_pretrain=audioset_pretrain,imagenet_pretrain=imagenet_pretrain)
+    #audio_model_ast = torch.nn.DataParallel(audio_model_ast)
     #audio_model.load_state_dict(sd, strict=False)
     return audio_model_ast
 
@@ -116,7 +130,7 @@ class Self_attention(Attention):
 
 class Encoder(nn.Module):
 
-    def __init__(self,depth_encoder,vit,trainable_encoder,avg=False):
+    def __init__(self,depth_encoder,vit,trainable_encoder,avg=False,depth_trainable=0):
         super().__init__()
         if avg:
             averaged_encoder = copy.deepcopy(vit.blocks[0])
@@ -126,7 +140,7 @@ class Encoder(nn.Module):
             self.encoderBlocks = vit.blocks[:depth_encoder]
 
         if trainable_encoder:
-            self.encoderBlocks.requires_grad_(True)
+            self.encoderBlocks[-depth_trainable:].requires_grad_(True)
         self.vit = vit
 
 
@@ -165,11 +179,13 @@ class attention_linear_model(nn.Module):
     :param depth_encoder: the number of blocks in the encoder
     :param depth_decoder: the number of layers in the decoder    """
 
-    def __init__(self, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, depth_encoder=1,
-                 depth_decoder=1,verbose=True, trainable_encoder = False,avg=False,audioset_only=False):
+    def __init__(self, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, depth_encoder=1,depth_trainable=1,
+                 depth_decoder=1,verbose=True, trainable_encoder = False,avg=False,audioset_only=False,
+                 audioset_pretrain=True,imagenet_pretrain=True,tiny=False):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         #device = "cpu"
 
+        print(device)
 
         super(attention_linear_model, self).__init__()
         assert timm.__version__ == '0.4.5', 'From AST model, not sure if really necessary'  # AST
@@ -185,7 +201,10 @@ class attention_linear_model(nn.Module):
                   .format(depth_encoder,depth_decoder))
 
         # automatically get the intermediate shape # AST
-        self.embed_dim=768
+        if tiny:
+            self.embed_dim=192
+        else:
+            self.embed_dim=768
         f_dim, t_dim = self.get_shape(fstride, tstride, input_fdim, input_tdim)
         num_patches = f_dim * t_dim
         if verbose == True:
@@ -194,7 +213,8 @@ class attention_linear_model(nn.Module):
 
         # the linear projection layer
         print("device: "+ str(device))
-        self.AST_model = pretrained_AST(audioset_only=audioset_only)
+        self.AST_model = pretrained_AST(audioset_only=audioset_only,audioset_pretrain=audioset_pretrain,imagenet_pretrain=imagenet_pretrain,
+                                        tiny=tiny)
         self.AST_model.requires_grad_(False)
         print("device: "+ str(device))
         #self.AST_model.v.to(device)
@@ -209,7 +229,7 @@ class attention_linear_model(nn.Module):
         #dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth_encoder)]  # stochastic depth decay rule # vIt
         #self.encoderBlocks = self.AST_model.v.blocks
 
-        self.encoder = Encoder(depth_encoder,self.AST_model.v,trainable_encoder,avg)
+        self.encoder = Encoder(depth_encoder,self.AST_model.v,trainable_encoder,avg,depth_trainable)
 
         self.decoder = Decoder(depth_decoder,self.embed_dim,act_layer=nn.GELU,drop=0.)
         self.decoder.requires_grad_(True)

ASTAE/src/preprocessing.py
@@ -21,7 +21,11 @@ def generate_spectrograms(input_base_directory,output_base_directory):
 
 
 def generate_dataframes(input_base_directory,output_base_directory):
-    for machine in os.listdir(input_base_directory):
+    #for machine in os.listdir(input_base_directory):
+    #for machine in ["fan","gearbox","pump","slider"]:
+    for machine in ["ToyCar","ToyTrain","valve"]:
+
+
         for domain in os.listdir(input_base_directory+"/"+machine):
             tensors_in_domain = None
             lables = []
@@ -62,20 +66,20 @@ raw_audio_base_directory="../../dev_data/"
 
 
 
-spectrograms_base_directory="../../dev_data_spectrograms/"
+#spectrograms_base_directory="../../dev_data_spectrograms/"
 #spectrograms_base_directory="../../dev_sample_spectrograms/"
 
-#spectrograms_base_directory="../../dev_data_spectrograms_server/"
+spectrograms_base_directory="../../dev_data_spectrograms_server/"
 
 
 
-dataframes_base_directory="../../dev_data_dataframes/"
+#dataframes_base_directory="../../dev_data_dataframes/"
 #dataframes_base_directory="../../dev_sample_dataframes/"
 
-#dataframes_base_directory="../../dev_data_dataframes_server/"
+dataframes_base_directory="../../dev_data_dataframes_server/"
 
 
-#generate_spectrograms(raw_audio_base_directory,spectrograms_base_directory)
+generate_spectrograms(raw_audio_base_directory,spectrograms_base_directory)
 generate_dataframes(spectrograms_base_directory,dataframes_base_directory)
 #test=torch.load("../../dev_sample_dataframes/debug_sample/train/dataframe.pt")
 #test2=torch.load("../../dev_data_dataframes/fan/train/dataframe.pt")

ASTAE/src/train.py
@@ -21,7 +21,9 @@ import numpy as np
 import pickle
 from torch.cuda.amp import autocast,GradScaler
 import pureAttentionAE
-import attention_linear
+#import attention_linear
+import attention_linearv2
+
 import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 from sklearn import metrics
@@ -70,7 +72,7 @@ def generate_roc_curve(y,labels,title):
     common.generate_ROC_curve(fpr_source, tpr_source, ROC_location_source)
 
 
-def train(version,model_title,audio_model, input_base_directory, batch_size, lr_encoder,lr_decoder,lr_patience=10,n_epochs=1,verbose=True,debug=False):
+def train(version,model_title,audio_model, input_base_directory, batch_size, lr_encoder,lr_decoder,machine,lr_patience=10,n_epochs=1,verbose=True,debug=False):
 
 
 
@@ -113,162 +115,176 @@ def train(version,model_title,audio_model, input_base_directory, batch_size, lr_
     #for machine in os.listdir(dataframes_base_directory):
     #for machine in ["fan"]:
 
-    for machine in ["fan","gearbox","pump","slider","ToyCar","ToyTrain","valve"]:
-    #for machine in ["pump","slider","ToyCar","ToyTrain","valve"]:
+    #for machine in ["gearbox","fan","pump","slider","ToyCar","ToyTrain","valve"]:
+    #for machine in ["ToyCar","ToyTrain","valve"]:
 
     #for machine in ["fan"]:
+    #for machine in ["ToyTrain","valve"]:
+
+
+    dataframe_dir = input_base_directory + machine+"/"
+    train_location = dataframe_dir+"train/dataframe.pt" #labels unecessary: all normal
+    validation_source_location = dataframe_dir+"source_test/dataframe.pt"
+    validation_source_labels_location = dataframe_dir+"source_test/labels.pt"
+    validation_target_location = dataframe_dir+"target_test/dataframe.pt"
+    validation_target_labels_location = dataframe_dir+"target_test/labels.pt"
+
+    X_train = torch.load(train_location)
+    X_validation_source = torch.load(validation_source_location)
+    X_validation_source_labels = torch.load(validation_source_labels_location)
+    X_validation_target = torch.load(validation_target_location)
+    X_validation_target_labels = torch.load(validation_target_labels_location)
+    if debug:
+        X_validation_source = X_validation_source[:6]
+        X_validation_source_labels = X_validation_source_labels[:6]
+        X_validation_target = X_validation_target[:6]
+        X_validation_target_labels = X_validation_target_labels[:6]
+    X_train.to(device, non_blocking=True)
+    X_validation_source.to(device, non_blocking=True)
+    X_validation_target.to(device, non_blocking=True)
+
+
+    nb_batches = round(len(X_train)/batch_size)
+
+    title = "DCASE21_"+model_title+"_"+machine+"_"+version
+
+    if debug:
+        log_folder = "runs/debug/"+machine+"/"
+    else:
+        log_folder = "runs/"+machine+"/"
 
 
 
-        dataframe_dir = input_base_directory + machine+"/"
-        train_location = dataframe_dir+"train/dataframe.pt" #labels unecessary: all normal
-        validation_source_location = dataframe_dir+"source_test/dataframe.pt"
-        validation_source_labels_location = dataframe_dir+"source_test/labels.pt"
-        validation_target_location = dataframe_dir+"target_test/dataframe.pt"
-        validation_target_labels_location = dataframe_dir+"target_test/labels.pt"
 
-        X_train = torch.load(train_location)
-        X_validation_source = torch.load(validation_source_location)
-        X_validation_source_labels = torch.load(validation_source_labels_location)
-        X_validation_target = torch.load(validation_target_location)
-        X_validation_target_labels = torch.load(validation_target_labels_location)
+    tb = SummaryWriter(log_folder+title)
+    sample_input = X_train[0:batch_size]
+    sample_input=sample_input.to(device)
+    tb.add_graph(audio_model,sample_input)
+
+    audio_model = torch.nn.DataParallel(audio_model)
+
+
+    train_loss_vals=  []
+    epoch = 1
+    while epoch < n_epochs:
+        print('---------------')
+        print(datetime.datetime.now())
+        print("current #epochs=%s, #steps=%s" % (epoch, global_step))
+        pos = 0
+        epoch_loss= []
         if debug:
-            X_validation_source = X_validation_source[:6]
-            X_validation_source_labels = X_validation_source_labels[:6]
-            X_validation_target = X_validation_target[:6]
-            X_validation_target_labels = X_validation_target_labels[:6]
-        X_train.to(device, non_blocking=True)
-        X_validation_source.to(device, non_blocking=True)
-        X_validation_target.to(device, non_blocking=True)
+            nb_batches = 2
 
 
-        nb_batches = round(len(X_train)/batch_size)
-
-        title = "DCASE21_"+model_title+"_"+machine+"_"+version
-
-        if debug:
-            log_folder = "runs/debug/"+machine+"/"
-        else:
-            log_folder = "runs/"+machine+"/"
-
-
-
-
-        tb = SummaryWriter(log_folder+title)
-        sample_input = X_train[0:batch_size]
-        sample_input=sample_input.to(device)
-        tb.add_graph(audio_model,sample_input)
-
-        train_loss_vals=  []
-        epoch = 0
-        while epoch < n_epochs + 1:
-            print('---------------')
-            print(datetime.datetime.now())
-            print("current #epochs=%s, #steps=%s" % (epoch, global_step))
-            pos = 0
-            epoch_loss= []
-            if debug:
-                nb_batches = 2
+        audio_model.train()
+        for i in range(nb_batches):
+            if (i%100 == 0):
+                print(i)
+            if (pos + batch_size>len(X_train)):
+                X_batch = X_train[pos:]
+            else:
+                X_batch = X_train[pos:pos + batch_size]
+            X_batch=X_batch.to(device, non_blocking=True)
+#            labels = labels.to(device, non_blocking=True)
+#                audio_model = audio_model.to(device)
+            with autocast():
+                audio_output,lin_proj_output = audio_model(X_batch)
+                loss = loss_fn(audio_output, lin_proj_output)
 
 
-            audio_model .train()
-            for i in range(nb_batches):
-                if (i%100 == 0):
-                    print(i)
-                if (pos + batch_size>len(X_train)):
-                    X_batch = X_train[pos:]
-                else:
-                    X_batch = X_train[pos:pos + batch_size]
-                X_batch=X_batch.to(device, non_blocking=True)
-    #            labels = labels.to(device, non_blocking=True)
-    #                audio_model = audio_model.to(device)
-                with autocast():
-                    audio_output,lin_proj_output = audio_model(X_batch)
-                    loss = loss_fn(audio_output, lin_proj_output)
+            optimizer.zero_grad()
+            if device=="cuda":
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+            else:
+                loss.backward()
+                optimizer.step()
+            epoch_loss.append(loss.item())
+
+            global_step += 1
+        avg_epoch_loss=sum(epoch_loss)/len(epoch_loss)
+        train_loss_vals.append(avg_epoch_loss)
+        tb.add_scalar('Loss/train', avg_epoch_loss, epoch)
+
+        scheduler.step(avg_epoch_loss)
 
 
-                optimizer.zero_grad()
-                if device=="cuda":
-                    scaler.scale(loss).backward()
-                    scaler.step(optimizer)
-                    scaler.update()
-                else:
-                    loss.backward()
-                    optimizer.step()
-                epoch_loss.append(loss.item())
+        audio_model.eval() # log validation accuracy during training (and without interfering with training)
 
-                global_step += 1
-            avg_epoch_loss=sum(epoch_loss)/len(epoch_loss)
-            train_loss_vals.append(avg_epoch_loss)
-            tb.add_scalar('Loss/train', avg_epoch_loss, epoch)
+        mse_source,auc_source,pauc_source = calc_AUC(X_validation_source,X_validation_source_labels,loss_fn,True,log=True,tb=tb,epoch=epoch,debug=debug)
+        mse_target,auc_target,pauc_target= calc_AUC(X_validation_target,X_validation_target_labels,loss_fn,False,log=True,tb=tb,epoch=epoch,debug=debug)
 
-            scheduler.step(avg_epoch_loss)
+        if epoch%50==0 and not debug:
+            torch.save(audio_model.state_dict(), "trained_models/intermediate_results/"+title+"_intermediate_"+str(epoch)+".pt")
 
-
-            audio_model.eval() # log validation accuracy during training (and without interfering with training)
-
-            mse_source,auc_source,pauc_source = calc_AUC(X_validation_source,X_validation_source_labels,loss_fn,True,log=True,tb=tb,epoch=epoch,debug=debug)
-            mse_target,auc_target,pauc_target= calc_AUC(X_validation_target,X_validation_target_labels,loss_fn,False,log=True,tb=tb,epoch=epoch,debug=debug)
-
-            if epoch%50==0 and not debug:
-                torch.save(audio_model.state_dict(), "trained_models/intermediate_results/"+title+"_intermediate_"+str(epoch)+".pt")
-
-            epoch += 1
+        epoch += 1
 
 
 
 
-        #figname = "DCASE21_"+machine+"_"+version+"_train"
-        #custom_plot(np.linspace(1, n_epochs, n_epochs).astype(int), train_loss_vals, figname,debug)
-        #for n_iter in range(100):
-         #   tb.add_scalar('Loss/train', np.random.random(), n_iter)
-        if not debug:
-            torch.save(audio_model.state_dict(), "trained_models/"+title+".pt")
+    #figname = "DCASE21_"+machine+"_"+version+"_train"
+    #custom_plot(np.linspace(1, n_epochs, n_epochs).astype(int), train_loss_vals, figname,debug)
+    #for n_iter in range(100):
+     #   tb.add_scalar('Loss/train', np.random.random(), n_iter)
+    if not debug:
+        torch.save(audio_model.state_dict(), "trained_models/"+title+".pt")
 
-        generate_roc_curve(mse_source,X_validation_source_labels,title+"_source")
-        generate_roc_curve(mse_target,X_validation_target_labels,title+"_target")
+    generate_roc_curve(mse_source,X_validation_source_labels,title+"_source")
+    generate_roc_curve(mse_target,X_validation_target_labels,title+"_target")
 
 
-        tb.close()
+    tb.close()
 
-        print(machine + " done")
 
 
 #TODO
 """
-- adjust hyper parameters?
-- put hyper parameters in yaml file
-
-extras not taken over from AST
--> warmup
--> save intermediate progress
+- put hyper parameters in yaml file (optional)
 """
 
 
 
 server = True
 
-audio_model = attention_linear.attention_linear_model(depth_encoder=12, trainable_encoder=False,avg=False,depth_decoder=1
-                                                    ,audioset_only=False)
-
-
-model_title = "attention_linear"
-
-
-if server:
-    input_base_directory = "../../dev_data_dataframes_server/"
-else:
-    input_base_directory = "../../dev_data_dataframes/"
 
 
 
-lr_encoder = 0.00005
-lr_decoder = 0.001
+#for machine in ["gearbox","valve","slider","ToyTrain","fan","pump","ToyCar"]:
+for machine in ["gearbox"]:
 
-batch_size=12 # AST
-version = "11,2"
-train(version,model_title,audio_model, input_base_directory, batch_size, lr_encoder,lr_decoder,lr_patience=5,n_epochs=200,verbose=True,
-      debug=False)
+    #versions = ["20,2","21,2","22,2","23,2","24,2"]
+    #for i in range(len(versions)):
+    #    depth_decoder = i +1
+
+
+
+    #audio_model = attention_linear.attention_linear_model(depth_encoder=1,depth_trainable=1, trainable_encoder=True,avg=False,depth_decoder=1
+    #                                                ,audioset_only=False,audioset_pretrain=False,imagenet_pretrain=False,
+    #                                                      tiny=False)
+    audio_model = attention_linearv2.attention_linear_model(depth_encoder=5,depth_trainable=1, trainable_encoder=True,avg=False,depth_decoder=1
+                                                    ,audioset_only=False,audioset_pretrain=True,imagenet_pretrain=True,
+                                                          tiny=False)
+
+    model_title = "attention_linear"
+
+
+    if server:
+        input_base_directory = "../../dev_data_dataframes_server/"
+    else:
+        input_base_directory = "../../dev_data_dataframes/"
+
+
+
+    lr_encoder = 0.00005
+    lr_decoder = 0.0001
+
+    batch_size=64  #128 makes gpu go out of memory
+    version = "B.1"
+
+    train(version,model_title,audio_model, input_base_directory, batch_size, lr_encoder,lr_decoder,lr_patience=5,n_epochs=200,verbose=True,
+          debug=False,machine=machine)
+    print(machine + " done")
 
 
 """

experiments/result_visualization/visualize_results.py
@@ -4,24 +4,27 @@ plt.style.use('ggplot')
 
 N = 7
 baseline_scores = (53.52, 50.95, 51.76, 50.51, 53.70,50.00,53.50)
-SVM_scores = (49.39,48.54,48.01,51.88,53.67,51.35,54.64)
-AE_scores = (50.87, 50.87, 49.44, 52.28, 60.00,55.64,56.12)
+#SVM_scores = (49.39,48.54,48.01,51.88,53.67,51.35,54.64)
+#AE_scores = (50.87, 50.87, 49.44, 52.28, 60.00,55.64,56.12)
 GMM_scores = (50.80, 49.47, 49.74, 51.46, 62.43,60.27,57.53)
-nn_single_layer_scores = (50.32, 50.61, 50.60, 50.23, 49.34,49.88,49.32)
-svm_scores = (51, 50.27, 50.53, 52.05, 50.15,49.62,50.3)
+#nn_single_layer_scores = (50.32, 50.61, 50.60, 50.23, 49.34,49.88,49.32)
+#svm_scores = (51, 50.27, 50.53, 52.05, 50.15,49.62,50.3)
+custom_scores = (52.12,49.64,51.81,51.77,61.72,56.49,55.55)
 
-factor = 4
+factor = 3
 
 width = 0.5
 
 
 ind = np.arange(N)
 plt.bar(ind, baseline_scores, width/factor, label='Baseline')
-plt.bar(ind + width/factor, SVM_scores, width/factor, label='One class SVM')
-plt.bar(ind + 2*width/factor, AE_scores, width/factor, label='AE')
-plt.bar(ind + 3*width/factor, GMM_scores, width/factor, label='GMM')
-plt.bar(ind + 4*width/factor, nn_single_layer_scores, width/factor, label='singe layer NN class')
-plt.bar(ind + 5*width/factor, svm_scores, width/factor, label='SVM class')
+#plt.bar(ind + width/factor, SVM_scores, width/factor, label='One class SVM')
+#plt.bar(ind + 2*width/factor, AE_scores, width/factor, label='AE')
+plt.bar(ind + width/factor, GMM_scores, width/factor, label='GMM')
+plt.bar(ind + 2*width/factor, custom_scores, width/factor, label='custom model')
+
+#plt.bar(ind + 4*width/factor, nn_single_layer_scores, width/factor, label='singe layer NN class')
+#plt.bar(ind + 5*width/factor, svm_scores, width/factor, label='SVM class')
 
 
 #plt.bar(ind + 2*width, women_means, width,

prototypes/src/ADAST.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 # ADAST: Anomaly Detector AST
 class adast(): # anomaly detection ast_model
-    def __init__(self,input_tdim = 1024,num_mel_bins=128,embedding_dimension=768,full_output=False):
+    def __init__(self,input_tdim = 1024,num_mel_bins=128,embedding_dimension=768,full_output=False,number_of_layers=None):
         # audioset input sequence length is 1024
         pretrained_mdl_path = '../../pretrained_models/audioset_10_10_0.4593.pth'
         # get the frequency and time stride of the pretrained model from its name
@@ -27,9 +27,9 @@ class adast(): # anomaly detection ast_model
         print(device)
         sd = torch.load(pretrained_mdl_path, map_location=device)
         if full_output:
-            audio_model_ast = models.ASTModel_full(input_tdim=input_tdim, fstride=fstride, tstride=tstride)
+            audio_model_ast = models.ASTModel_full(input_tdim=input_tdim, fstride=fstride, tstride=tstride,number_of_layers=number_of_layers)
         else:
-            audio_model_ast = models.ASTModel(input_tdim=input_tdim, fstride=fstride, tstride=tstride)
+            audio_model_ast = models.ASTModel(input_tdim=input_tdim, fstride=fstride, tstride=tstride,number_of_layers=number_of_layers)
         self.audio_model = torch.nn.DataParallel(audio_model_ast)
         self.audio_model.load_state_dict(sd, strict=False)
         self.num_mel_bins=num_mel_bins
@@ -76,22 +76,37 @@ def generate_lables_and_pd_dataframe(input_directory,format="one_class_svm",cust
             if tensors_in_domain == None:
                 tensors_in_domain = loaded_tensor
             else:
+                loaded_tensor.to("cpu")
+                tensors_in_domain.to('cpu')
                 tensors_in_domain = torch.cat((tensors_in_domain, loaded_tensor))
-    px = pd.DataFrame(tensors_in_domain.detach().numpy())
+    px = pd.DataFrame(tensors_in_domain.detach().cpu().numpy())
     return lables,px
 
 
 # generate intermediate tensors and store as .pt files
-full_output = True
+full_output = False
+server = True
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-adast_mdl = adast(full_output=full_output)
+nb_layers = 5
+adast_mdl = adast(full_output=full_output,number_of_layers=nb_layers)
 base_directory="../../dev_data/"
-if full_output:
-    output_base_directory="../dev_data_embeddings_full/"
+
+
+if nb_layers ==None:
+    if full_output:
+        output_base_directory="../../dev_data_embeddings_full/"
+    else:
+        output_base_directory="../../dev_data_embeddings/"
 else:
-    output_base_directory="../dev_data_embeddings/"
-for machine in os.listdir(base_directory):
+    if full_output:
+        output_base_directory="../../dev_data_embeddings_full_"+str(nb_layers)+"layers/"
+    else:
+        output_base_directory="../../dev_data_embeddings_"+str(nb_layers)+"layers/"
+
+
+
+
+for machine in ["gearbox","valve","slider","ToyTrain","fan","pump","ToyCar"]:
     for domain in os.listdir(base_directory+"/"+machine):
         input_directory = base_directory + machine + "/" + domain
         output_directory = output_base_directory + machine+'/'+domain
@@ -108,21 +123,23 @@ for machine in os.listdir(base_directory):
 
 
 # save as dataframe
-"""
-embedding_base_directory="../dev_data_embeddings/"
-for machine in os.listdir(embedding_base_directory):
+
+#embedding_base_directory="../../dev_data_embeddings/"
+
+embedding_base_directory=output_base_directory
+for machine in ["gearbox","valve","slider","ToyTrain","fan","pump","ToyCar"]:
     if os.path.isdir(embedding_base_directory+"/"+machine):
         print(machine)
         for domain in os.listdir(embedding_base_directory + "/" + machine):
             machine_dir=embedding_base_directory + machine
             input_directory = embedding_base_directory + machine + "/" + domain
             if os.path.isdir(input_directory):
-                X=load_embeddings(input_directory)
+                X=common.load_embeddings(input_directory)
                 pickle_location=input_directory+"/"+"dataframe.pkl"
                 X.to_pickle(pickle_location)
                 print(X.shape)
         print(machine+" "+domain+" done")
-"""
+
 
 #embedding_base_directory = "../dev_data_embeddings/"
 #common.combine_embeddings(embedding_base_directory)

prototypes/src/common.py
@@ -123,6 +123,7 @@ def load_embeddings(input_directory):
         if filename.endswith(".pt"):
             file_location = input_directory + "/" + filename
             loaded_tensor = torch.load(file_location)
+            loaded_tensor=loaded_tensor.to("cpu")
             if tensors_in_domain == None:
                 tensors_in_domain = loaded_tensor
             else:
@@ -312,10 +313,22 @@ def select_dirs(param, mode):
     """
     if mode:
         logger.info("load_directory <- development")
-        query = os.path.abspath("{base}/*".format(base=param["dev_directory"]))
+        query = os.path.abspath("../{base}/*".format(base=param["dev_directory"]))
     else:
-        logger.info("load_directory <- evaluation")
-        query = os.path.abspath("{base}/*".format(base=param["eval_directory"]))
+        raise ("not implemented yet")
+        #logger.info("load_directory <- evaluation")
+        #query = os.path.abspath("../{base}/*".format(base=param["eval_directory"]))
+    dirs = sorted(glob.glob(query))
+    dirs = [f for f in dirs if os.path.isdir(f)]
+    return dirs
+
+
+def select_dirs_AST(param, mode):
+    if mode:
+        logger.info("load_directory <- development")
+        query = os.path.abspath("../{base}/*".format(base=param["ast_data_directory"]))
+    else:
+        raise ("not implemented yet")
     dirs = sorted(glob.glob(query))
     dirs = [f for f in dirs if os.path.isdir(f)]
     return dirs

prototypes/src/models/ast_model_full_output.py
@@ -44,11 +44,12 @@ class ASTModel_full(nn.Module):
     :param audioset_pretrain: if use full AudioSet and ImageNet pretrained model
     :param model_size: the model size of AST, should be in [tiny224, small224, base224, base384], base224 and base 384 are same model, but are trained differently during ImageNet pretraining.
     """
-    def __init__(self, label_dim=527, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, imagenet_pretrain=True, audioset_pretrain=False, model_size='base384', verbose=True):
+    def __init__(self, label_dim=527, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, imagenet_pretrain=True, audioset_pretrain=False, model_size='base384', verbose=True,
+                 number_of_layers = None):
 
         super(ASTModel_full, self).__init__()
         assert timm.__version__ == '0.4.5', 'Please use timm == 0.4.5, the code might not be compatible with newer versions.'
-
+        self.number_of_layers = number_of_layers
         if verbose == True:
             print('---------------AST Model Summary---------------')
             print('ImageNet pretraining: {:s}, AudioSet pretraining: {:s}'.format(str(imagenet_pretrain),str(audioset_pretrain)))
@@ -175,8 +176,13 @@ class ASTModel_full(nn.Module):
         x = torch.cat((cls_tokens, dist_token, x), dim=1)
         x = x + self.v.pos_embed
         x = self.v.pos_drop(x)
-        for blk in self.v.blocks:
-            x = blk(x)
+        if self.number_of_layers == None:
+            for blk in self.v.blocks:
+                x = blk(x)
+        else:
+            for blk in self.v.blocks[:self.number_of_layers]:
+                x = blk(x)
+
         x = self.v.norm(x)
         x = x[:,2:] #drop class and distillation token
         #x = (x[:, 0] + x[:, 1]) / 2

prototypes/src/models/ast_models.py
@@ -29,6 +29,9 @@ class PatchEmbed(nn.Module):
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
+        temp = self.proj(x)
+        temp2=self.proj(x).flatten(2)
+        temp3=temp2.unflatten(2,(12,101))
         x = self.proj(x).flatten(2).transpose(1, 2)
         return x
 
@@ -44,10 +47,12 @@ class ASTModel(nn.Module):
     :param audioset_pretrain: if use full AudioSet and ImageNet pretrained model
     :param model_size: the model size of AST, should be in [tiny224, small224, base224, base384], base224 and base 384 are same model, but are trained differently during ImageNet pretraining.
     """
-    def __init__(self, label_dim=527, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, imagenet_pretrain=True, audioset_pretrain=False, model_size='base384', verbose=True):
+    def __init__(self, label_dim=527, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, imagenet_pretrain=True, audioset_pretrain=False, model_size='base384', verbose=True,
+                 number_of_layers=None):
 
         super(ASTModel, self).__init__()
         assert timm.__version__ == '0.4.5', 'Please use timm == 0.4.5, the code might not be compatible with newer versions.'
+        self.number_of_layers = number_of_layers
 
         if verbose == True:
             print('---------------AST Model Summary---------------')
@@ -175,8 +180,12 @@ class ASTModel(nn.Module):
         x = torch.cat((cls_tokens, dist_token, x), dim=1)
         x = x + self.v.pos_embed
         x = self.v.pos_drop(x)
-        for blk in self.v.blocks:
-            x = blk(x)
+        if self.number_of_layers == None:
+            for blk in self.v.blocks:
+                x = blk(x)
+        else:
+            for blk in self.v.blocks[:self.number_of_layers]:
+                x = blk(x)
         x = self.v.norm(x)
         x = (x[:, 0] + x[:, 1]) / 2
         #x = self.mlp_head(x)